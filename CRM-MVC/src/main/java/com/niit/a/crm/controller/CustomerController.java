deb3abd40b24d7121479dc687622dae9e2762a14
deepanshu <deepanshujindal1021997@gmail.com>
2020-03-27 16:58:48 +0530

    Customer Control

--- CRM-MVC/src/main/java/com/niit/a/crm/controller/CustomerController.java
+++ CRM-MVC/src/main/java/com/niit/a/crm/controller/CustomerController.java
@@ -1,21 +1,42 @@
 package com.niit.a.crm.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.niit.a.crm.model.Customer;
+import com.niit.a.crm.service.CustomerService;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	
+	@Autowired
+	private CustomerService customerService;
+
 	@GetMapping("/showForm")
 	public String showFormForAdd(ModelMap theModel) {
 		Customer theCustomer = new Customer();
 		theModel.addAttribute("customer", theCustomer);
-		System.out.println("Hello WORLD");
+		System.out.println("ShowForm");
 		return "customer-form";
+	}
+
+	@PostMapping("/saveCustomer")
+	public String saveCustomer(@ModelAttribute("customer") Customer theCustomer) {
+	 System.out.println("Hello");
+		customerService.saveCustomer(theCustomer);
+		return "success";
+	}
+	@GetMapping("/list") // @GetMapping - method level , default GET method
+	public String listCustomers(ModelMap theModel) {
+	List<Customer> theCustomers = customerService.getCustomers();
+	theModel.addAttribute("customers", theCustomers);
+	return "list-customers";
 	}
 }
